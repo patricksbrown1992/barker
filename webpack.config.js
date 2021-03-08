7f2c4d9e46e0f3d12e1c6ad031cd203ef4943af7
patricksbrown1992 <patricksbrown1992@gmail.com>
2021-03-07 22:39:31 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    mode: 'development',
+    entry: './frontend/barker.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+            test: /\.jsx?$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/env', '@babel/react']
+                }
+            },
+        }, 
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader", "postcss-loader"]
+          }
+    ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    }
+};
